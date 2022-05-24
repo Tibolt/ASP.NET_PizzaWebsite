99aaffc9a45d44b26c3134d40f02c9a445bc210b
Tibolt <tibo.ak015@gmail.com>
2022-05-25 00:11:55 +0200

    Added: site with pizza list to order

---
+++ Razor_PizzaWeb/Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_PizzaWeb.Pages.Forms
+{
+    public class CustomPizzaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Razor_PizzaWeb/Pages/PizzaList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_PizzaWeb.Pages
+{
+    public class PizzaListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
